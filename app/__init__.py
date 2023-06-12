94eb74caf51fea18e6ac81dc9cd0bd4364b00028
Lucas Frazao <94771586+Lucas-Frazao@users.noreply.github.com>
2023-06-12 10:45:21 -0500

    New features

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from flask_migrate import Migrate  # database
-from flask_sqlalchemy import SQLAlchemy  # database
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager  # makes sure user is logged in before visiting other parts of the application
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
 
--- app/models.py
+++ app/models.py
@@ -1,12 +1,38 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 from app import db
+from flask_login import UserMixin  # includes generic implementations that are appropriate for most user model classes
+from app import login  # Flask-Login user loader function
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    # methods bellow are for password hashing
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader  # method gives an id that Flask-Login uses to keep track of User session status
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
