28d9a8bb53e5feb17bd20c6c2477c09e4b68b69d
Lucas Frazao <94771586+Lucas-Frazao@users.noreply.github.com>
2023-08-25 20:04:00 -0500

    New features

--- app/models.py
+++ app/models.py
@@ -3,15 +3,26 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from flask_login import UserMixin  # includes generic implementations that are appropriate for most user model classes
 from app import login  # Flask-Login user loader function
+from datetime import datetime
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
 
 
-class User  (UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     level = db.Column(db.String(120), index=True)
-    events = db.relationship('Event', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,18 +34,33 @@ class User  (UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.filter_by(user_id=self.id)
+        return followed.order_by(Post.timestamp.desc())
+
 
 @login.user_loader  # method gives an id that Flask-Login uses to keep track of User session status
 def load_user(id):
     return User.query.get(int(id))
 
 
-class Event(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(64))
-    description = db.Column(db.String(140))
-    date = db.Column(db.String(64))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Event {}>'.format(self.description)
+        return '<Post {}>'.format(self.body)
